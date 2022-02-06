fad58c75d956ae455dbb4f919cdf7c9e79dc4f33
nehalBaigSystemLtd <96332398+nehalBaigSystemLtd@users.noreply.github.com>
2022-02-06 05:10:23 +0500

    Update app.test.js

--- test/app.test.js
+++ test/app.test.js
@@ -10,14 +10,16 @@ const width = 640;
 const height = 480;
 
 
-let driver=new webdriver.Builder().forBrowser('chrome').setChromeOptions(
-  new chrome.Options().headless().windowSize({width, height})).build();
+// let driver=new webdriver.Builder().forBrowser('chrome').setChromeOptions(
+//   new chrome.Options().headless().windowSize({width, height})).build();
+
 // let driver=new webdriver.Builder().forBrowser('chrome').build();
-// var service = new chrome.ServiceBuilder(chromedriver.path).build();
-// chrome.setDefaultService(service);
-// var driver = new webdriver.Builder()
-// .withCapabilities(webdriver.Capabilities.chrome())
-// .build();
+var service = new chrome.ServiceBuilder(chromedriver.path).build();
+chrome.setDefaultService(service);
+
+var driver = new webdriver.Builder()
+.withCapabilities(webdriver.Capabilities.chrome())
+.build();
 
 describe('Form Page', function() {
   before(function () {
