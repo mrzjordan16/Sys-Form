954ad22160b219e5bd7a85029e47d64df49e2c22
nehalBaigSystemLtd <96332398+nehalBaigSystemLtd@users.noreply.github.com>
2022-03-04 05:16:35 +0500

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-unzip buildOutput.zip
-cd buildOutput
-npm install pm2 -g
-npm install  --production
+#unzip buildOutput.zip
+#cd buildOutput
+#npm install pm2 -g
+#npm install  --production
+ls -lat
