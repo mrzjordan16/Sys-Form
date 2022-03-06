6e0064e4ea2a2c41b63b1357f980ae386b336b8e
nehalBaigSystemLtd <nehal.baig@systemsltd.com>
2022-03-06 18:47:34 +0000

    update

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-sudo rm -rf /home/ubuntu/nodejs
-mkdir /home/ubuntu/nodejs
+#sudo rm -rf /home/ubuntu/nodejs
+#mkdir /home/ubuntu/nodejs
 ls -lat
-cp build.zip /home/ubuntu/nodejs/build.zip
-cd /home/ubuntu/nodejs/
-sudo unzip build.zip
-sudo npm install pm2 -g
-sudo npm install  --production
-ls /home/ubuntu/nodejs/ -lat
+#cp build.zip /home/ubuntu/nodejs/build.zip
+#cd /home/ubuntu/nodejs/
+#sudo unzip build.zip
+#sudo npm install pm2 -g
+#sudo npm install  --production
+#ls /home/ubuntu/nodejs/ -lat
 
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-cd /home/ubuntu/nodejs/
+#cd /home/ubuntu/nodejs/
 ls -lat
-echo ============================
-ls /home/ubuntu/nodejs/ -lat
-sudo npm run pm_start
-ls /home/ubuntu/nodejs/ -lat
+#echo ============================
+#ls /home/ubuntu/nodejs/ -lat
+#sudo npm run pm_start
+#ls /home/ubuntu/nodejs/ -lat
